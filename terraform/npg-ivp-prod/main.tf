68f8e7ed1f7411e0a72485d88647e889154db997
Mukesh.Sahu <msahu201989@gmail.com>
2024-04-22 23:25:07 +0530

    Terraform

--- terraform/npg-ivp-prod/main.tf
+++ terraform/npg-ivp-prod/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "awswithlinux" {
-  #  provider = aws.cross_account
+  provider = aws.cross_account
   bucket   = "napiercrossaccount39"
 
   tags = {
--- terraform/npg-ivp-prod/provider.tf
+++ terraform/npg-ivp-prod/provider.tf
@@ -1,6 +1,14 @@
-#provider "aws" {
-#  region = "us-east-1"
-#}
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "cross_account"
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::851725296471:role/TerraformCrossAccountRole"
+  }
+}
 #
 #provider "aws" {
 #  alias  = "cross_account"
@@ -10,24 +18,16 @@
 #  }
 #}
 #
-provider "aws" {
-  alias  = "cross_account"
-  region = "us-east-1"
-  assume_role {
-    role_arn = "arn:aws:iam::851725296471:role/TerraformCrossAccountRole"
-  }
-}
-
-# Set default provider for aws_vpc and aws_subnet to the cross-account provider
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-
-  providers {
-    aws = aws.cross_account
-  }
-}
+## Set default provider for aws_vpc and aws_subnet to the cross-account provider
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 3.0"
+#    }
+#  }
+#
+#  providers {
+#    aws = aws.cross_account
+#  }
+#}
