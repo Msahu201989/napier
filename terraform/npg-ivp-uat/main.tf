ee4e11fdca36329ec08505ec6ef44192d3881402
Mukesh.Sahu <msahu201989@gmail.com>
2024-04-22 21:13:09 +0530

    Terraform

--- terraform/npg-ivp-uat/main.tf
+++ terraform/npg-ivp-uat/main.tf
@@ -1,11 +1,20 @@
-resource "aws_s3_bucket" "awswithlinux99" {
-  #  provider = aws.cross_account
-  bucket   = "napiercrossaccount39"
+#resource "aws_s3_bucket" "awswithlinux99" {
+#  #  provider = aws.cross_account
+#  bucket   = "napiercrossaccount39"
+#
+#  tags = {
+#    "Application" = "Geneva_Dev"
+#    "Stack"       = "DEV"
+#    "Name"        = "vpc_geneva_dev"
+#    "Module"      = "Geneva"
+#  }
+#}
 
-  tags = {
-    "Application" = "Geneva_Dev"
-    "Stack"       = "DEV"
-    "Name"        = "vpc_geneva_dev"
-    "Module"      = "Geneva"
-  }
+
+provider "aws" {
+  region = "use-east-1"
 }
+
+resource "aws_instance" "sample" {
+  ami           = "ami-00d48a21603b2119b"
+  instance_type = "t3.micro"
